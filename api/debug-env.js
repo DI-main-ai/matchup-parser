ad0f1b285498393bc85d58ea233b1818bdac3237
DI-main-ai <daanesh.ibrahim@gmail.com>
2025-09-19 16:01:56 -0500

    Update debug-env.js

--- api/debug-env.js
+++ api/debug-env.js
@@ -1,5 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    has_OPENAI_API_KEY: Boolean(process.env.OPENAI_API_KEY)
-  });
+  res.status(200).json({ has_OPENAI_API_KEY: !!process.env.OPENAI_API_KEY });
 }
